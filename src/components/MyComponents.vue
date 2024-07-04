5cb09a844c26c592c5ff4b7477c329378ea4c0f7
Carminelm <carminelm54@gmail.com>
2024-07-04 17:14:40 +0200

    add title

---
+++ src/components/MyComponents.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name:"MyComponents",
+}
+
+</script>
+
+<template>
+
+  <section>
+    <h1>“La mia prima app con Vite!”</h1>
+
+ </section>
+
+
+</template>
+
+<style scoped>
+
+</style>
